4f927a66dd4a590b870abb7abe86899e5d32936f
ach <ignaciomenchaca@gmail.com>
2018-05-21 01:00:16 -0300

    Clase 4

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
+// Formas de acceder a los datos de la URL
+
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const wes = { name: 'Wes', age: 30, cool: true };
+  // res.send('Hey! It works!');
+  // res.json(wes);
+
+  // Tomar los datos que vienen en la url como parametros
+  // "req" tiene toda la informacion solicitada
+  // "res" tiene todos los metodos para devolver datos
+  // "req.query" se encarga de los parametros
+
+  // res.send(`Nombre: ${req.query.name} - Edad ${req.query.age}`);
+  res.json(req.query);
+});
+
+// "req.params" accede a los campos variables de la url:
+// las variables se indican como ":variable"
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;
