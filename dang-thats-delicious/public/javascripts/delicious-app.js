3d6fc0425d14aa58a32fce8ed1501db075226035
ach <ignaciomenchaca@gmail.com>
2018-07-02 00:38:43 -0300

    Clase 16

--- dang-thats-delicious/public/javascripts/delicious-app.js
+++ dang-thats-delicious/public/javascripts/delicious-app.js
@@ -1,3 +1,9 @@
+// Punto de entrada para todo el javascript
+
 import '../sass/style.scss';
 
 import { $, $$ } from './modules/bling';
+
+import autocomplete from './modules/autocomplete';
+
+autocomplete($('#address'), $('#lat'), $('#lng'));
