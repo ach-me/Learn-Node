10cadb61179c438b0ba61361f8061973ed334a76
ach <ignaciomenchaca@gmail.com>
2018-09-05 22:00:34 -0300

    Clase 27

--- dang-thats-delicious/controllers/authController.js
+++ dang-thats-delicious/controllers/authController.js
@@ -1,5 +1,10 @@
 // Paquete para manejar el logueo
 const passport = require('passport');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const promisify = require('es6-promisify');
+
+const User = mongoose.model('User');
 
 // Middleware
 exports.login = passport.authenticate('local', {
@@ -13,16 +18,95 @@ exports.logout = (req, res) => {
   req.logout();
   req.flash('success', 'You are logged out!');
   res.redirect('/');
-}
+};
 
 // Middleware. Verificar si el usuario esta logueado
 exports.isLoggedIn = (req, res, next) => {
   // 1. Check if user is authenticated
   if (req.isAuthenticated()) {
     next(); // he is logged in
-    return;
   } else {
     req.flash('error', 'Oops you must be logged in to do that');
     res.redirect('/login');
   }
-}
+};
+
+// Cuando alguien requiere resetear password
+exports.forgot = async (req, res) => {
+  // 1. See if that user exists
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    req.flash('error', 'A password reset has been mailed to you.');
+    return res.redirect('/login');
+  }
+
+  // 2. Set resets tokens and expiry on their account
+  // crypto permite generar un string random para generar el token
+  // Estos dos campos hay que agregarlos al Schema de User.js
+  user.resetPasswordToken = crypto.randomBytes(20).toString('hex');
+  user.resetPasswordExpires = Date.now() + 3600000; // 1 hour from now
+  // Wait until the user is saved
+  await user.save();
+
+  // 3. Send them an email with the token
+  const resetURL = `http://${req.headers.host}/account/reset/${user.resetPasswordToken}`;
+  // Say the user it has worked
+  req.flash('success', `You have been emailed a password reset link: ${resetURL}`);
+
+  // 4. Redirect to the login page
+  res.redirect('/login');
+};
+
+exports.reset = async (req, res) => {
+  // res.json(req.params);
+  // 1. Check if the token exists
+  // Search the user
+  const user = await User.findOne({
+    resetPasswordToken: req.params.token,
+    resetPasswordExpires: { $gt: Date.now() }, // "gt" greater than... en MongoDB
+  });
+  if (!user) {
+    req.flash('error', 'Password reset is invalid or has expired');
+    return res.redirect('/login');
+  }
+
+  // 2. Is the token expired
+  res.render('reset', { title: 'Reset your password' });
+};
+
+exports.confirmedPasswords = (req, res, next) => {
+  if (req.body.password === req.body['password-confirm']) {
+    next();
+    return;
+  }
+  req.flash('error', 'Passwords do not match');
+  res.redirect('back');
+};
+
+exports.update = async (req, res) => {
+  const user = await User.findOne({
+    resetPasswordToken: req.params.token,
+    resetPasswordExpires: { $gt: Date.now() }, // "gt" greater than... en MongoDB
+  });
+
+  if (!user) {
+    req.flash('error', 'Password reset is invalid or has expired');
+    return res.redirect('/login');
+  }
+
+  // Actualizar password
+  // El metodo "setPassword" esta disponible porque se importo en User.js
+  // Como este metodo no retorna una promesa, se utilizar el paquete es6-promisify
+  const setPassword = promisify(user.setPassword, user);
+  // Set the new password, hash it, salt it...
+  await setPassword(req.body.password);
+
+  // Quitar los fields de la base MongoDB
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpires = undefined;
+  // Para que se vean reflejados estos cambios hay que guardar el usuario
+  const updatedUser = await user.save();
+  await req.login(updatedUser);
+  req.flash('success', 'Your password has been reset');
+  res.redirect('/');
+};
--- dang-thats-delicious/models/User.js
+++ dang-thats-delicious/models/User.js
@@ -24,18 +24,18 @@ const userSchema = new Schema({
     require: 'Please supply a name',
     trim: true,
   },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date,
 });
 
 // Virtual field. En mongodb es un campo que puede ser generado sin necesidad de definirlo en el schema, en lugar de tener que guardar en la base de datos
 // Crear campo virtual
-userSchema.virtual('gravatar').get(function () {
+userSchema.virtual('gravatar').get(function() {
   // gravatar usa el algoritmo "mp5". Toma el email del usuario y lo hashea
   const hash = md5(this.email);
 
-  return `https://gravatar.com/avatar/${hash}?s=200`
-
+  return `https://gravatar.com/avatar/${hash}?s=200`;
 });
-
 
 // Agregar plugin al schema
 // agregar todos los metodos y campos necesarios para permitir autenticacion al schema. 'email' es el campo de login
