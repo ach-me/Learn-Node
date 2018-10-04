767221e7cf11f461bca0868b10ebead247d43a76
ach <ignaciomenchaca@gmail.com>
2018-10-03 21:18:48 -0300

    Clase 37

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
+const reviewController = require('../controllers/reviewController');
 const { catchErrors } = require('../handlers/errorHandlers'); // Object destructuring. Importa solo el metodo de igual nombre
 
 // Formas de acceder a los datos de la URL
@@ -103,6 +104,7 @@ router.post(
 );
 router.get('/map', storeController.mapPage);
 router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts));
+router.post('/reviews/:id', authController.isLoggedIn, catchErrors(reviewController.addReview));
 
 /**
  * API ENDPOINTS
